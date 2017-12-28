abb015f4b11ee1fd2e50525f45be8d9f53b66a00
pantaelaman <calvin@theophoffs.com>
2017-12-28 12:57:42 -0500

    Secondary Commit

--- src/slowcube/SlowCube.java
+++ src/slowcube/SlowCube.java
@@ -5,6 +5,7 @@ import net.canarymod.Canary;
 import net.canarymod.commandsys.*;
 import net.canarymod.chat.MessageReceiver;
 import net.canarymod.api.entity.living.humanoid.Player;
+import net.canarymod.api.world.position.Location;
 import com.pragprog.ahmine.ez.EZPlugin;
 
 public class SlowCube extends EZPlugin {
@@ -17,7 +18,7 @@ public class SlowCube extends EZPlugin {
     if (caller instanceof Player) { 
       Player me = (Player)caller;
       // Put your code after this line:
-
+		
       // ...and finish your code before this line.
     }
   }
